b353c1b45d55f9d4787c083a62e2dea1fd3f9e8d
usfit <dima050595fedorov@yandex.ru>
2022-08-01 23:58:29 +0300

    исправление поп-ап

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,18 +1,33 @@
 let burgerOpen = document.querySelector('.burger__icon--open');
 let burgerClose = document.querySelector('.burger__icon--close');
 let navigation = document.querySelector('.navigation');
-let container = document.querySelector('.container');
+let mask = document.querySelector('.mask');
+let swiper = document.querySelectorAll('.swiper-wrapper');
+// let container = document.querySelector('.container');
 
+let openCloseMask = function() {
+    mask.classList.toggle('mask--close');
+    for (let i = 0; i < swiper.length; i++) {
+        swiper[i].classList.toggle('swiper-wrapper');
+    };
+};
 
 burgerOpen.addEventListener('click', function(evt) {
     evt.preventDefault;
-    container.classList.toggle('container--close');
+    openCloseMask();
     navigation.classList.toggle('navigation--open');
 });
 
 burgerClose.addEventListener('click', function(evt) {
     evt.preventDefault;
-    container.classList.toggle('container--close');
+    openCloseMask();
     navigation.classList.toggle('navigation--open');
+});
+
+mask.addEventListener('click', function() {
+    if (navigation.classList.contains('navigation--open')) {
+        navigation.classList.toggle('navigation--open');
+        openCloseMask();
+    }
 });
 
--- src/js/feedback.js
+++ src/js/feedback.js
@@ -2,58 +2,79 @@ let feedbackOpen = document.querySelector('.chat__icon--open');
 let feedbackOpenBurger = document.querySelector('.burger-chat__icon--open');
 let feedbackClose = document.querySelector('.feedback__icon--close');
 let feedbackCallClose = document.querySelector('.feedback__call--close');
-
 let feedbackCallOpen = document.querySelector('.call__icon--open');
 let feedbackCallOpenBurger = document.querySelector('.burger-call__icon--open');
 
 let feedback = document.querySelector('.feedback');
 let feedbackCall = document.querySelector('.feedback--call');
 
-let container = document.querySelector('.container');
+let mask = document.querySelector('.mask');
+let swiper = document.querySelectorAll('.swiper-wrapper');
+
 let navigation = document.querySelector('.navigation');
 
 let openClose = function(feedbackClass) {
-    container.classList.toggle('container--close');
     feedbackClass.classList.toggle('feedback--open');
-    navigation.classList.toggle('container--close');
 };
 
+let openCloseMask = function() {
+    mask.classList.toggle('mask--close');
+    for (let i = 0; i < swiper.length; i++) {
+        swiper[i].classList.toggle('swiper-wrapper');
+    }
+};
+
+mask.addEventListener('click', function() {
+    if (feedback.classList.contains('feedback--open')) {
+        openClose(feedback);
+        if (window.matchMedia('(max-width: 1119.9px)').matches) {
+            openCloseMask();
+        }
+    } else if (feedbackCall.classList.contains('feedback--open')) {
+        openClose(feedbackCall);
+        if (window.matchMedia('(max-width: 1119.9px)').matches) {
+            openCloseMask();
+        }
+    }
+});
+
 feedbackCallOpen.addEventListener('click', function(evt) {
-    evt.preventDefault;
+    evt.preventDefault();
+    openCloseMask();
     openClose(feedbackCall);
 });
 
 feedbackCallOpenBurger.addEventListener('click', function(evt) {
-    if (window.matchMedia('(max-width: 1119.9px)').matches) {
-        evt.preventDefault;
-        feedbackCall.classList.toggle('feedback--open');
-        navigation.classList.toggle('navigation--open');
-    } else {
-        openClose(feedbackCall);
+    openClose(feedbackCall);
+    navigation.classList.toggle('navigation--open');
+    if (window.matchMedia('(min-width: 1120px)').matches) {
+        openCloseMask();
     }
 });
 
 feedbackCallClose.addEventListener('click', function(evt) {
-    evt.preventDefault;
+    evt.preventDefault();
     openClose(feedbackCall);
+    openCloseMask();
 });
 
 feedbackOpen.addEventListener('click', function(evt) {
-    evt.preventDefault;
+    evt.preventDefault();
     openClose(feedback);
+    openCloseMask();
 });
 
 feedbackClose.addEventListener('click', function(evt) {
-    evt.preventDefault;
+    evt.preventDefault();
     openClose(feedback);
+    openCloseMask();
 });
 
 feedbackOpenBurger.addEventListener('click', function(evt) {
-    if (window.matchMedia('(max-width: 1119.9px)').matches) {
-        evt.preventDefault;
-        feedback.classList.toggle('feedback--open');
-        navigation.classList.toggle('navigation--open');
-    } else {
-        openClose(feedback);
+    openClose(feedback);
+    navigation.classList.toggle('navigation--open');
+    if (window.matchMedia('(min-width: 1120px)').matches) {
+        openCloseMask();
     }
 });
+
